2eeb354c529d8080f41eb996734e5112e3517f8b
Niko Felger <niko.felger@gmail.com>
2021-08-04 22:14:03 +0200

    Add 'production' redirect_uri

--- oauth2client.js
+++ oauth2client.js
@@ -2,14 +2,19 @@ const path = require("path");
 
 const { google } = require("googleapis");
 
+const env = process.env.ENV || "development";
+
 const client_id = "46213974342-aas8rl309kots0cp6jtphn20judhdckl.apps.googleusercontent.com";
 const client_secret = process.env.GOOGLE_CLIENT_SECRET
-const redirect_uris = ["http://localhost:8888/oauth2callback"];
+const redirect_uris = {
+  development: "http://localhost:8888/oauth2callback",
+  production: "https://calstat.herokuapp.com/oauth2callback"
+};
 
 const oauth2client = new google.auth.OAuth2(
   client_id,
   client_secret,
-  redirect_uris
+  redirect_uris[env]
 );
 
 module.exports = oauth2client;
